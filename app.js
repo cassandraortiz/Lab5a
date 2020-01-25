ba681140e986f917b28b88d1b19c4555406b63f8
Cassandra <Cassy@Stephens-MBP.localdomain>
2020-01-25 14:56:56 -0800

    Promblem #3 Solved

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
     var sum = a + b;
     var string = 'The sum of ' + a + ' and ' + b + ' is ' + sum + '.';
+    //return sum;
     return [sum, string];
 }
 
@@ -32,7 +33,8 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
     var product = a * b;
     var string = "The product of " + a + " and " + b + " is " + product + ".";
-    return [product, string];
+   return [product, string];
+    //return product;
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -50,14 +52,26 @@ Fourth element: "The product of 4 and 7 and 5 is 140."
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+// var productA = multiply(4, 7)[0];
+// var answerProduct = multiply(productA, 5)[0];
+// console.log(multiply(number1, sum2)[0]);
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var sumA = sum(a, b)[0];
+    var answerSum = sum(sumA, c)[0];
 
+    var productA = multiply(a, b)[0];
+    var answerProduct = multiply(productA, c)[0];
+    var sumString = a + " and " + b + " and " + c + " sum to " + answerSum + ".";
+    var productString = "The product of " + a + " and " + b + " and " + c + " is " + answerProduct + ".";
+
+//returns and array [sum of 3 numbers, product of 3#, stingsum, stringproduct ]
+    return [answerSum, answerProduct, sumString, productString]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
