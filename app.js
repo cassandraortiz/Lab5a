ebf107bfab38ffb22e2d7896add41526829c6a19
Cassandra <Cassy@Stephens-MBP.localdomain>
2020-01-25 16:14:27 -0800

    Problem5 Solved

--- app.js
+++ app.js
@@ -124,11 +124,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+    var arrProductA = multiply(multArr[0], multArr[1])[0];
+    var answerArrProduct = multiply(arrProductA, multArr[2])[0];
 
+    var testArrayString2 = 'The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + answerArrProduct + '.'
+    // console.log(answerArraySum);
+    return [answerArrProduct,testArrayString2];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
